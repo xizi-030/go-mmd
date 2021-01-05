2b1007e13983d8b39a948b4be6fb5c72918e45a8
ryant1986 <ryan.glenn.thompson@gmail.com>
2021-01-05 13:34:36 -0600

    Update mmd_test.go

--- mmd/mmd_test.go
+++ mmd/mmd_test.go
@@ -19,13 +19,19 @@ func TestEchoCall(t *testing.T) {
 		t.Skip("integration tests disabled")
 	}
 
-	mmdc, err := LocalConnect()
+	mmdc, err := Connect()
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer closeConnection(t, mmdc)
+
 	t.Log("Created mmd connection:", mmdc)
 	resp, err := mmdc.Call("echo", "Howdy Doody")
 	t.Logf("Response: %+v\nError: %v\n", resp, err)
+}
+
+func closeConnection(t *testing.T, mmdc Conn) {
 	t.Log("Shutting down MMD connection")
-	mmdc.close()
+	err := mmdc.close()
+	t.Logf("Close error: %v\n", err)
 }

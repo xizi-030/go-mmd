f212e68fcb2c944601a6201e43e798a8bc18dd5a
ryant1986 <ryan.glenn.thompson@gmail.com>
2021-02-25 14:44:37 -0600

    add env lookup methods to mmd.go

--- mmd/mmd.go
+++ mmd/mmd.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	logpkg "log"
 	"os"
+	"strconv"
 )
 
 var log = logpkg.New(os.Stdout, "[mmd] ", logpkg.LstdFlags|logpkg.Lmicroseconds)
@@ -12,7 +13,24 @@ var mmdUrl = "localhost:9999"
 func init() {
 	flag.StringVar(&mmdUrl, "mmd", mmdUrl, "Sets default MMD Url")
 	//env takes precedence
-	if mmdUrlFromEnv, ok := os.LookupEnv("MMD_URL_OVERRIDE"); ok {
-		mmdUrl = mmdUrlFromEnv
+	mmdUrl = getEnv("MMD_URL_OVERRIDE", mmdUrl)
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
 	}
+	return fallback
+}
+
+func getEnvBool(key string, fallback bool) bool {
+	if value, ok := os.LookupEnv(key); ok {
+		var result, err = strconv.ParseBool(value)
+		if err != nil {
+			panic("Invalid value for boolean env var " + key +
+				" - must be one of: 1, t, T, TRUE, true, True, 0, f, F, FALSE, false, False")
+		}
+		return result
+	}
+	return fallback
 }

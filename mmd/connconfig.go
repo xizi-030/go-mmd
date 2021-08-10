51cae324f87871e1ecce90b93a23844163ba6770
xizi-030 <88722403+xizi-030@users.noreply.github.com>
2021-08-10 08:21:41 -0500

    fix composite connection retry

--- mmd/connconfig.go
+++ mmd/connconfig.go
@@ -56,7 +56,7 @@ func _create_connection(cfg *ConnConfig) (Conn, error) {
 		mmdc = createConnection(cfg)
 	}
 
-	err := mmdc.createSocketConnection(false)
+	err := mmdc.createSocketConnection(false, true)
 	if err != nil {
 		return nil, err
 	}
